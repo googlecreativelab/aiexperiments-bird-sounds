cdf3cc27d510b6848c29c1dffe9c396bd9696611
Yotam Mann <yotammann@gmail.com>
2016-11-14 15:15:19 -0800

    updating youtube video

--- app/core/Data.js
+++ app/core/Data.js
@@ -43,7 +43,7 @@ var birdSuggestions = require("raw!data/autosuggest_bird.txt");
 }());
 
 var Data = module.exports = {
-	videoId: '26IEFT6wJtI',
+	videoId: '31PWjb7Do1s',
 	totalPoints: birdCoords.length,
 	filterStates:null,
 	filteredList: [],
