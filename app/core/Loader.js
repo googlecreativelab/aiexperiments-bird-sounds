5510f9875ff3cb89715b9f3c77a6ef4e525135fe
Andy Kenward <hello@andykenward.com>
2017-10-18 18:16:01 +0100

    fix: scss folder for loader.scss

--- app/core/Loader.js
+++ app/core/Loader.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-require("style/loader.scss");
+require("../../style/loader.scss");
 
 var ImageLoader = require("image-progress");
 
